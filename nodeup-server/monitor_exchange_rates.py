83615a136b49d8f0a157e735aad4ee0dedaf7f9b
Max Kaye <m@xk.io>
2015-08-16 10:11:08 +1000

    implement exchange rate stuff

--- nodeup-server/monitor_exchange_rates.py
+++ nodeup-server/monitor_exchange_rates.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 
+import time
+
+import requests
+
+from models import exchange_rate
+
+while True:
+    res = requests.get('https://bitpay.com/api/rates/usd')
+    response = res.json()
+    if 'code' in response:
+        if response['code'] == "USD":
+            exchange_rate.set(response['rate'])
+    time.sleep(30)
