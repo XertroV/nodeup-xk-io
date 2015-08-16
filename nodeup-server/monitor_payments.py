0354a4299d175edf10520c93b0d6574c0276434d
Max Kaye <m@xk.io>
2015-08-16 19:43:07 +1000

    update monitor_payments to not use bitcoind and only mark a block as checked after completing all checks

--- nodeup-server/monitor_payments.py
+++ nodeup-server/monitor_payments.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
 import time
+import logging
 
 from bitcoinrpc import connect_to_local
 from pycoin.tx import Tx
-from blockchain.blockexplorer import get_tx
+from blockchain.blockexplorer import get_tx, get_latest_block
 
 from models import last_block_checked, unprocessed_txs, Account, addr_to_uid, nodes_recently_updated, txs, all_addresses
 from constants import REQUIRED_CONFIRMATIONS, COIN
 from digitalocean import calc_node_minutes
 
-bitcoind = connect_to_local()
+logging.basicConfig(level=logging.INFO)
 
 while True:
-    best_block_hash = bitcoind.getbestblockhash()
-    top_height = bitcoind.getblock(best_block_hash)['height']
+    lastest_block = get_latest_block()
+    best_block_hash = lastest_block.hash
+    top_height = lastest_block.height
     if best_block_hash == last_block_checked.get():
         time.sleep(10)  # only do this at most once per block
         continue
-    last_block_checked.set(best_block_hash)
+    logging.info('Latest block: %s' % best_block_hash)
 
     for txid in unprocessed_txs.members():
         tx = Tx.tx_from_hex(txs[txid])
@@ -40,3 +42,5 @@ while True:
                 account.add_msg('Increased total paid by %.8f to %.8f' % (out.coin_value / COIN, account.total_coins.get() / COIN))
                 account.add_msg('Increased node life by %d minutes; expiring around %d' % (node_minutes_d, account.get_expiry().isoformat()))
                 nodes_recently_updated.append(account.uid)
+
+    last_block_checked.set(best_block_hash)
