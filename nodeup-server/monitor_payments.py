34da3f2f03c744c9b6579431b14bd1e45a4a10d1
Max Kaye <m@xk.io>
2015-08-17 11:06:20 +1000

    fix syntax and type issues

--- nodeup-server/monitor_payments.py
+++ nodeup-server/monitor_payments.py
@@ -10,6 +10,7 @@ from blockchain.blockexplorer import get_tx, get_latest_block
 from models import last_block_checked, unprocessed_txs, Account, addr_to_uid, nodes_recently_updated, txs, all_addresses
 from constants import REQUIRED_CONFIRMATIONS, COIN
 from digitalocean_custom import calc_node_minutes
+from wallet import hash_to_hex
 
 logging.basicConfig(level=logging.INFO)
 
@@ -23,14 +24,15 @@ if __name__ == '__main__':
             continue
         logging.info('Latest block: %s' % best_block_hash)
 
-        for txid in unprocessed_txs.members():
-            tx = Tx.tx_from_hex(txs[txid])
+        for tx_hash in unprocessed_txs.members():
+            txid = hash_to_hex(tx_hash)
+            tx = Tx.tx_from_hex(txs[tx_hash].decode())
             tx_blockchain = get_tx(txid)
             logging.info('Checking %s' % txid)
             if tx_blockchain.block_height == -1:
                 continue
             if top_height - tx_blockchain.block_height >= REQUIRED_CONFIRMATIONS:
-                unprocessed_txs.remove(txid)
+                unprocessed_txs.remove(tx_hash)
                 for out in tx.txs_out:
                     address = out.address()
                     if address not in all_addresses:
@@ -40,7 +42,7 @@ if __name__ == '__main__':
                     account.total_coins.incr(satoshis)
                     node_minutes_d = calc_node_minutes(satoshis)
                     account.total_minutes.incr(node_minutes_d)
-                    account.add_msg('Detected payment via txid: %s' % (tx['txid'],))
+                    account.add_msg('Detected payment via txid: %s' % (txid,))
                     account.add_msg('Increased total paid by %.8f to %.8f' % (out.coin_value / COIN, account.total_coins.get() / COIN))
                     account.add_msg('Increased node life by %d minutes; expiring around %d' % (node_minutes_d, account.get_expiry().isoformat()))
                     nodes_recently_updated.append(account.uid)
--- nodeup-server/wallet.py
+++ nodeup-server/wallet.py
@@ -21,18 +21,19 @@ def process_tx_initial(tx_obj: Tx):
         logging.info('Found irrelevant tx %s' % hash_to_hex(tx_obj.hash()))
         return
 
-    txid = tx_obj.hash()
-    if txid in known_txs:
+    tx_hash = tx_obj.hash()
+    txid = tx_hash(tx_hash)
+    if tx_hash in known_txs:
         return
-    known_txs.add(txid)
-    txs[txid] = tx_obj.as_hex()
+    known_txs.add(tx_hash)
+    txs[tx_hash] = tx_obj.as_hex()
     for out in tx_obj.txs_out:
         address = out.bitcoin_address()
         if address in all_addresses and address is not None:
-            unprocessed_txs.add(txid)
+            unprocessed_txs.add(tx_hash)
             uid = addr_to_uid[address]
             account = Account(uid)
-            account.txs.add(txid)
+            account.txs.add(tx_hash)
             account.unconf_minutes.incr(calc_node_minutes(satoshi_amount=out.coin_value, exchange_rate=exchange_rate.get()))
             account.add_msg('Found tx for %.08f, %s' % (out.coin_value / COIN, txid))
 
