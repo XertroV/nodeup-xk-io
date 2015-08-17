a80c25d871441ed66df0bcb8c14df56b3e1a2d85
Max Kaye <m@xk.io>
2015-08-17 11:16:06 +1000

    derp, fix funciton call

--- nodeup-server/wallet.py
+++ nodeup-server/wallet.py
@@ -22,7 +22,7 @@ def process_tx_initial(tx_obj: Tx):
         return
 
     tx_hash = tx_obj.hash()
-    txid = tx_hash(tx_hash)
+    txid = hash_to_hex(tx_hash)
     if tx_hash in known_txs:
         return
     known_txs.add(tx_hash)
