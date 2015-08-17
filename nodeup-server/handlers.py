7228f6b0523910dac9d14334f5c01602bd8756d7
Max Kaye <m@xk.io>
2015-08-18 09:49:46 +1000

    formatting error

--- nodeup-server/handlers.py
+++ nodeup-server/handlers.py
@@ -32,7 +32,7 @@ def handle(method, **params):
         tip = account.tip.get()
 
         amount = actually_charge(months, tip, exchange_rate.get())
-        response['uri'] = "bitcoin:%s?amount=%.8f&label=nodeup.xk.io%20sponsored%20node%20funding%20address." % (account.address, amount)
+        response['uri'] = "bitcoin:%s?amount=%.8f&label=nodeup.xk.io%%20sponsored%%20node%20funding%%20address." % (account.address, amount)
         response['status'] = 'Payment received.' if account.unconf_minutes.get() > 0 else 'Waiting for payment...'
 
     elif method == '':
