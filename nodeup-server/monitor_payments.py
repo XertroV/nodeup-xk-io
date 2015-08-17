dbbb158e75aaaba7bcd2cd9e602c4861c6e12ddb
Max Kaye <m@xk.io>
2015-08-17 15:14:29 +1000

    increment global stats when relevant

--- nodeup-server/monitor_payments.py
+++ nodeup-server/monitor_payments.py
@@ -7,7 +7,7 @@ from bitcoinrpc import connect_to_local
 from pycoin.tx import Tx
 from blockchain.blockexplorer import get_tx, get_latest_block
 
-from models import last_block_checked, unprocessed_txs, Account, addr_to_uid, nodes_recently_updated, txs, all_addresses, exchange_rate
+from models import last_block_checked, unprocessed_txs, Account, addr_to_uid, nodes_recently_updated, txs, all_addresses, exchange_rate, total_nodeminutes
 from constants import REQUIRED_CONFIRMATIONS, COIN
 from digitalocean_custom import calc_node_minutes
 from wallet import hash_to_hex
@@ -43,6 +43,7 @@ if __name__ == '__main__':
                     account.total_coins.incr(satoshis)
                     node_minutes_d = calc_node_minutes(satoshis, exchange_rate=exchange_rate.get())
                     account.total_minutes.incr(node_minutes_d)
+                    total_nodeminutes.incr(node_minutes_d)
                     nodes_recently_updated.append(account.uid)
                     account.add_msg('Detected payment via txid: %s' % (txid,))
                     account.add_msg('Increased total paid by %.8f to %.8f (considering tip of %d %%)' % (satoshis / COIN, account.total_coins.get() / COIN, account.tip.get() * 100))
