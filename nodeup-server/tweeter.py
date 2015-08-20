a3c14794fc5130e464b09f87479501f2e5cd702a
Max Kaye <m@xk.io>
2015-08-21 08:48:51 +1000

    fix update_status bug and slight refactor

--- nodeup-server/tweeter.py
+++ nodeup-server/tweeter.py
@@ -8,18 +8,21 @@ import tweepy
 from models import twitter_access_token, twitter_access_secret, twitter_consumer_secret, twitter_consumer_key, tweet_queue
 
 
-auth = tweepy.OAuthHandler(twitter_consumer_key.get(), twitter_consumer_secret.get())
-auth.set_access_token(twitter_access_token.get(), twitter_access_secret.get())
-api = tweepy.API(auth)
+def get_api():
+    auth = tweepy.OAuthHandler(twitter_consumer_key.get(), twitter_consumer_secret.get())
+    auth.set_access_token(twitter_access_token.get(), twitter_access_secret.get())
+    return tweepy.API(auth)
+
 
 if __name__ == "__main__":
+    api = get_api()
     while True:
         if len(tweet_queue) > 0:
             tweet = tweet_queue.popleft().decode()
             try:
-                api.update_status(tweet)
+                api.update_status(status=tweet)
             except Exception as e:
                 tweet_queue.prepend(tweet)
                 logging.error('Tweet failed: %s gave error %s' % (tweet, repr(e)))
                 raise e
-        time.sleep(1)
+        time.sleep(5)
