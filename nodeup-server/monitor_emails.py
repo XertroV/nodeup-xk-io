525ab746c48dd750f921087f77ed54dc4be04ba0
Max Kaye <m@xk.io>
2015-08-21 11:25:29 +1000

    Use send_message not send()

--- nodeup-server/monitor_emails.py
+++ nodeup-server/monitor_emails.py
@@ -10,12 +10,13 @@ from models import mandrill_api_key, mandrill_username, email_queue_json
 smtp = smtplib.SMTP('smtp.mandrillapp.com', port=587)
 smtp.login(mandrill_username.get(), mandrill_api_key.get())
 
-while True:
-    if len(email_queue_json) > 0:
-        email = json.loads(email_queue_json.popleft().decode())
-        msg = MIMEText(email['body'])
-        msg['Subject'] = email['subject']
-        msg['From'] = email['from']
-        msg['To'] = email['to']
-        smtp.send(msg)
-    time.sleep(1)
+if __name__ == '__main__':
+    while True:
+        if len(email_queue_json) > 0:
+            email = json.loads(email_queue_json.popleft().decode())
+            msg = MIMEText(email['body'])
+            msg['Subject'] = email['subject']
+            msg['From'] = email['from']
+            msg['To'] = email['to']
+            smtp.send_message(msg)
+        time.sleep(1)
