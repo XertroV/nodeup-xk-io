d29ab7f326cd25c42c102bfa2634faed516b65d5
Max Kaye <m@xk.io>
2015-08-22 15:17:18 +1000

    rename options from digitalocean to vultr

--- nodeup-server/admin.py
+++ nodeup-server/admin.py
@@ -13,9 +13,8 @@ from monitor_nodes import process_next_creation, configure_droplet
 logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--ssh-management-key', help='Set fingerprint of ssh management key.', type=str, default='')
-parser.add_argument('--ssh-auditor-key', help='Set fingerprint of ssh management key.', type=str, default='')
-parser.add_argument('--digitalocean-api-key', help='Set digitalocean api key.', type=str, default='')
+parser.add_argument('--ssh-management-key', help='Set key-id of ssh management key (vultr).', type=str, default='')
+parser.add_argument('--vultr-api-key', help='Set vultr api key.', type=str, default='')
 parser.add_argument('--xpub', help='Set xpub key', type=str, default='')
 parser.add_argument('--test-uid-create-node', help='Provide UID to test node creation', type=str, default='')
 parser.add_argument('--msgs-for-uid', help='provide uid get msgs', type=str, default='')
@@ -41,8 +40,8 @@ if args.ssh_management_key != '':
 if args.ssh_auditor_key != '':
     ssh_auditor_key.set(args.ssh_auditor_key)
 
-if args.digitalocean_api_key != '':
-    vultr_api_key.set(args.digitalocean_api_key)
+if args.vultr_api_key != '':
+    vultr_api_key.set(args.vultr_api_key)
 
 if args.xpub != '':
     xpub.set(args.xpub)
